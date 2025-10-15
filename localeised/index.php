bea6bb1638b150f1cb343f45f46980ce32ef55fe
Michaelninder <fabianeternis@gmail.com>
2025-10-15 11:56:14 +0200

    feat: Update some functions (useing config()) and co ...

--- localeised/index.php
+++ localeised/index.php
@@ -1,24 +1,30 @@
 <?php
 
-
 $config = include __DIR__ . '/config.php';
 
-$locale = $_GET['lang'] ?? $config['default_locale'];
+$locale = $_GET['lang'] ?? config('default_locale');
 
-if (!in_array($locale, $config['available_locales'], true)) {
-    $locale = $config['default_locale'];
+if (!in_array($locale, config('available_locales'), true)) {
+    $locale = config('default_locale');
 }
 
 $translations = include __DIR__ . "/lang/{$locale}.php";
 
-function translate($key, $translations)
+function config($key, $default = null)
 {
+    global $config;
+    return $config[$key] ?? $default;
+}
+
+function translate($key)
+{
+    global $translations;
     return $translations[$key] ?? $key;
 }
 
-function getLocaleName($localeShort, $config)
+function getLocaleName($localeShort)
 {
-    return $config['locales_map'][$localeShort] ?? $localeShort;
+    return config("locales_map")[$localeShort] ?? $localeShort;
 }
 
 function revertBool($value)
@@ -26,21 +32,19 @@ function revertBool($value)
     return !$value;
 }
 
-$debug_mode = $config['debug_mode'] ?? false;
-
-function generateRoute($route, $config, $params = [])
+function generateRoute($route, $params = [])
 {
     global $locale;
 
     if (!isset($params['lang'])) {
-        if ($locale !== $config['default_locale']) {
+        if ($locale !== config('default_locale')) {
             $params['lang'] = $locale;
         }
-    } /*else {
-        if ($params['lang'] === $config['default_locale']) {
+    } /* else {
+        if ($params['lang'] === config('default_locale')) {
             unset($params['lang']);
         }
-    }*/
+    } */
 
     $queryString = http_build_query($params);
 
@@ -52,11 +56,7 @@ function generateRoute($route, $config, $params = [])
 
     return $url;
 }
-function config($key, $default = null)
-{
-    global $config;
-    return $config[$key] ?? $default;
-}
+
 ?>
 
 <!DOCTYPE html>
@@ -64,21 +64,21 @@ function config($key, $default = null)
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title><?= translate('page_title', $translations) ?></title>
+    <title><?= translate('page_title') ?></title>
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <header>
-        <h1><?= translate('welcome_message', $translations) ?></h1>
+        <h1><?= translate('welcome_message') ?></h1>
     </header>
 
     <nav>
         <ul>
-            <?php foreach ($config['available_locales'] as $availableLocale): ?>
+            <?php foreach (config('available_locales') as $availableLocale): ?>
                 <li>
-                    <a href="<?= generateRoute('', $config, ['lang' => $availableLocale]) ?>"
+                    <a href="<?= generateRoute('', ['lang' => $availableLocale]) ?>"
                        class="<?= $locale === $availableLocale ? 'active' : '' ?>">
-                        <?= getLocaleName($availableLocale, $config) ?>
+                        <?= getLocaleName($availableLocale) ?>
                     </a>
                 </li>
             <?php endforeach; ?>
@@ -86,12 +86,10 @@ function config($key, $default = null)
     </nav>
 
     <main>
-        <h3><?= translate('greeting', $translations) ?></h3>
+        <h3><?= translate('greeting') ?></h3>
     </main>
 </body>
 </html>
 
-
-
-<pre><?php print_r($translations, revertBool($debug_mode)); ?></pre>
-<pre><?php print_r($config, revertBool($debug_mode)); ?></pre>
+<pre><?php print_r($translations, revertBool(config('debug_mode'))); ?></pre>
+<pre><?php print_r($config, revertBool(config('debug_mode'))); ?></pre>
